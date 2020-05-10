97cad9109e5e3143065195dbdeaa43517e662a3b
Nishanth Solomon <nsolomo2@asu.edu>
2020-05-09 19:05:13 -0700

    Added code to create dataset

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from sia_auto_generation.sia_auto_generation import SiaAutoGeneration
 
 def parse_args():
     parser = argparse.ArgumentParser(description='bio-sia-auto-generation')
-    parser.add_argument(
-        '--path', type=str, default='./dataset/sentence4.tsv')
     parser.add_argument('--num_sia_generation', type=int, default=20)
     parser.add_argument('--cuda_device', type=int, default=-1)
     return parser.parse_args()
@@ -20,3 +18,4 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('./sia_auto_generation/conf/sia_configuration.conf')
     sia_auto_generation = SiaAutoGeneration(config, cuda_device)
+    sia_auto_generation.create_dataset(args.num_sia_generation)
--- sia_auto_generation/sia_auto_generation.py
+++ sia_auto_generation/sia_auto_generation.py
@@ -1,21 +1,42 @@
 import requests
+import json
 from sci_spacy.sci_spacy import SciSpacy
 from nltk_word_net.nltk_word_net import NltkWordnet
+from dataset_reader.dataset_reader import SiaDatasetReader
 
 
 class SiaAutoGeneration():
     def __init__(self, config, cuda_device: False):
         query_generation_config = config['query_generation']
         sci_spacy_config = config['sci_spacy']
+        dataset_reader_config = config['dataset_reader']
 
         self.query_generation_url = query_generation_config['url']
         self.sci_spacy = SciSpacy(sci_spacy_config, cuda_device)
         self.nltk_word_net = NltkWordnet()
+        self.sia_dataset_reader = SiaDatasetReader(dataset_reader_config)
+
+    def create_dataset(self, num_sia_generation):
+        count = 0
+        for sentence_4 in self.sia_dataset_reader.read_sentence_4():
+            if (count == num_sia_generation):
+                break
+            self.create_data(sentence_4)
+            count += 1
 
     def create_data(self, sentence_4):
         query = self.get_query(sentence_4)
+        sentence_3 = self.get_sentence_3()
+        sentence_2 = self.get_sentence_2()
+        sentence_1 = self.get_sentence_1()
 
-        return query
+        json_data = {'query': query, 'sentence_4': sentence_4,
+                     'sentence_3': sentence_3, 'sentence_2': sentence_2, 'sentence_1': sentence_1}
+        json_data = json.dumps(json_data, indent=4, sort_keys=False)
+        print(json_data)
+
+        self.sia_dataset_reader.write_dataset(
+            query, sentence_4, sentence_3, sentence_2, sentence_1)
 
     def get_query(self, sentence_4):
         # Obtain query using a Sentence 4 via generative model API
@@ -25,10 +46,10 @@ class SiaAutoGeneration():
         return query
 
     def get_sentence_3(self):
-        pass
+        return ''
 
     def get_sentence_2(self):
-        pass
+        return ''
 
     def get_sentence_1(self):
-        pass
+        return ''
